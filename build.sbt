d93863a311f4394a0843dded8846fe826b9aba53
sergey-lagutin <sergey.v.lagutin@gmail.com>
2015-05-28 08:35:46 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "Testing scala"
+
+version := "1.0"
+
+scalaVersion := "2.9.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
+
---
+++ src/test/scala/com/oreilly/testingscala/AlbumTest.scala
@@ -0,0 +1,12 @@
+package com.oreilly.testingscala
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class AlbumTest extends FunSpec with ShouldMatchers {
+	describe("Album") {
+		it ("can add Artist object to the album") {
+			val album = new Album("Thriller", 1981, new Artist("Michael", "Jackson"))
+		}
+	}
+}

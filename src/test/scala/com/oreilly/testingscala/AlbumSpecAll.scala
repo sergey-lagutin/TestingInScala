170b01174e8d9ab8722b134fa2f7b2d53ccf814a
sergeylagutin <sl@anahoret.com>
2015-05-30 22:29:27 +0300

    Add AlbumSpecAll.

---
+++ src/test/scala/com/oreilly/testingscala/AlbumSpecAll.scala
@@ -0,0 +1,31 @@
+package com.oreilly.testingscala
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{Tag, GivenWhenThen, FunSpec}
+
+class AlbumSpecAll extends FunSpec with ShouldMatchers with GivenWhenThen {
+  describe("An Album") {
+    it("can add an Artist to album at construction time", Tag("construction")) {
+      given("The Thriller by Michael Jackson")
+      val album = new Album("Thriller", 1981, Artist("Michael", "Jackson"))
+
+      when("the artist of album is obtained")
+      val artist = album.artist
+
+      then("the artist should be an instance of Artist")
+      artist.isInstanceOf[Artist] should be(true)
+
+
+      and("the artist's first name and last name should be Michael Jackson")
+      artist.firstName should be("Michael")
+      artist.lastName should be("Jackson")
+
+      info("This is still pending, since there may be more to accomplish in this test")
+      pending
+    }
+
+    ignore("can add a Producer to an album at construction time") {
+      // TODO: Add some logic to add a producer
+    }
+  }
+}

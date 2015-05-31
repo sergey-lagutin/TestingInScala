360cf23f2163ebe76bf6eab47a17ebca646313ed
sergeylagutin <sl@anahoret.com>
2015-05-31 11:40:51 +0300

    Add AlbumWordSpec.

--- src/main/scala/com/oreilly/testingscala/Album.scala
+++ src/main/scala/com/oreilly/testingscala/Album.scala
@@ -1,5 +1,18 @@
 package com.oreilly.testingscala
 
-class Album(val title: String, val year: Int, val artist: Artist)
+class Act
 
-case class Artist(firstName: String, lastName: String)
+case class Artist(firstName: String, lastName: String) extends Act
+
+class Band(name: String, members: Artist*) extends Act
+
+class Album(val title: String, val year: Int, val acts: List[Act]) {
+  require(acts.size > 0)
+
+  def this(title: String, year: Int, act: Act) {
+    this(title, year, List(act))
+  }
+
+  def artist = acts.head.asInstanceOf[Artist]
+}
+
---
+++ src/test/scala/com/oreilly/testingscala/AlbumWordSpec.scala
@@ -0,0 +1,23 @@
+package com.oreilly.testingscala
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class AlbumWordSpec extends WordSpec with ShouldMatchers {
+  "An Album" when {
+    "created" should {
+      "accept the title, the year and a Band as parameter, and be able to read those parameters back" in {
+        new Album("Hotel California", 1977,
+          new Band("The Eagles", new Artist("Don", "Henley"), new Artist("Glenn", "Frey")))
+      }
+    }
+  }
+
+  "An Album" should {
+    "throw an IllegalArgumentException if there are no acts when created" in {
+      intercept[IllegalArgumentException] {
+        new Album("The Joy", 1980, List())
+      }
+    }
+  }
+}

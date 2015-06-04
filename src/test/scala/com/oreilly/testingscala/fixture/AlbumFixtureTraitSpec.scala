d7a612bdce4c142be8763e10a7832e9aec3dd950
sergey-lagutin <sergey.v.lagutin@gmail.com>
2015-06-04 16:47:20 +0300

    Add samples of fixture based on trait.

---
+++ src/test/scala/com/oreilly/testingscala/fixture/AlbumFixtureTraitSpec.scala
@@ -0,0 +1,21 @@
+package com.oreilly.testingscala.fixture
+
+import com.oreilly.testingscala.{Album, Band}
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class AlbumFixtureTraitSpec extends FunSpec with ShouldMatchers {
+
+  trait AlbumFixture {
+    val letterFromHome = new Album("Letter from Home", 1989,
+      new Band("Pat Metheny Group"))
+  }
+
+  describe("The Letter From Home Album by Pat Metheny") {
+    it("should get the year 1989 from the album") {
+      new AlbumFixture {
+        letterFromHome.year should be(1989)
+      }
+    }
+  }
+}
